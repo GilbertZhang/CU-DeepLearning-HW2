9db1e51d6303fbd31a02dc2347b061bb79c6c07b
Gilbert <gilbert@Gilberts-MacBook-Pro.local>
2018-10-07 02:47:41 -0400

    gg

--- hw2baseline.1.py
+++ hw2baseline.1.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from keras.metrics import categorical_accuracy
 from keras import backend as K
 import tensorflow as tf
+from attention import AttentionDecoder
 
 
 # The custom accuracy metric used for this task
@@ -33,10 +34,10 @@ def onehot_to_seq(oh_seq, index):
 def print_results(x, y_, revsere_decoder_index):
     # print("input     : " + str(x))
     # print("prediction: " + str(onehot_to_seq(y_, revsere_decoder_index).upper()))
-    return str(onehot_to_seq(y_, revsere_decoder_index).upper())
+    print(str(onehot_to_seq(y_, revsere_decoder_index).upper()))
 
 # Computes and returns the n-grams of a particualr sequence, defaults to trigrams
-def seq2ngrams(seqs, n = 2):
+def seq2ngrams(seqs, n = 3):
     return np.array([[seq[i : i + n] for i in range(len(seq))] for seq in seqs])
 
 
@@ -78,34 +79,16 @@ test_input_data = sequence.pad_sequences(test_input_data, maxlen = maxlen_seq, p
 n_words = len(tokenizer_encoder.word_index) + 1
 n_tags = len(tokenizer_decoder.word_index) + 1
 
-# input = Input(shape = (maxlen_seq,))
-encoder_input = Input(shape=(maxlen_seq,))
-decoder_input = Input(shape=(maxlen_seq,))
+input = Input(shape = (maxlen_seq,))
 
 # Defining an embedding layer mapping from the words (n_words) to a vector of len 128
-x = Embedding(input_dim = n_words, output_dim = 64, input_length = maxlen_seq)(encoder_input)
-encoder = Bidirectional(LSTM(64, return_sequences=True, unroll=True))(x)
-encoder_last = encoder[:,-1,:]
-print(encoder)
-x1 = Embedding(input_dim = n_tags, output_dim = 64, input_length = maxlen_seq)(encoder_input)
-decoder = Bidirectional(LSTM(64, return_sequences=True, unroll=True))(x, initial_state=[encoder_last, encoder_last])
-from keras.layers import Activation, dot, concatenate
+x = Embedding(input_dim = n_words, output_dim = 128, input_length = maxlen_seq)(input)
 
-# Equation (7) with 'dot' score from Section 3.1 in the paper.
-# Note that we reuse Softmax-activation layer instead of writing tensor calculation
-attention = dot([decoder, encoder], axes=[2, 2])
-attention = Activation('softmax', name='attention')(attention)
-context = dot([attention, encoder], axes=[2,1])
-decoder_combined_context = concatenate([context, decoder])
-output = TimeDistributed(Dense(128, activation="tanh"))(decoder_combined_context)
-y = TimeDistributed(Dense(n_tags, activation="softmax"))(output)
-
-
-# # # Defining a bidirectional LSTM using the embedded representation of the inputs
-# x = Bidirectional(LSTM(units = 256, return_sequences = True, recurrent_dropout = 0.1))(x)
-
-# # # A dense layer to output from the LSTM's64 units to the appropriate number of tags to be fed into the decoder
-# y = Dense(n_tags, activation = "softmax")(x)
+# Defining a bidirectional LSTM using the embedded representation of the inputs
+x = Bidirectional(LSTM(units = 64, return_sequences = True, recurrent_dropout = 0.1))(x)
+y = AttentionDecoder(x, output_dim=n_tags, trainable=True)(x)
+# A dense layer to output from the LSTM's64 units to the appropriate number of tags to be fed into the decoder
+# y = TimeDistributed(Dense(n_tags, activation = "softmax"))(x)
 
 # Defining the model as a whole and printing the summary
 model = Model(input, y)
@@ -129,23 +112,19 @@ Non-trainable params: 0
 """
 
 # Setting up the model with categorical x-entropy loss and the custom accuracy function as accuracy
-model.compile(optimizer = "Adagrad", loss = "categorical_crossentropy", metrics = ["accuracy", accuracy])
+model.compile(optimizer = "rmsprop", loss = "categorical_crossentropy", metrics = ["accuracy", accuracy])
 
 # Splitting the data for train and validation sets
 X_train, X_val, y_train, y_val = train_test_split(train_input_data, train_target_data, test_size = .1, random_state = 0)
 
 # Training the model on the training data and validating using the validation set
-model.fit(X_train, y_train, batch_size = 128, epochs = 10, validation_data = (X_val, y_val), verbose = 1)
+model.fit(X_train, y_train, batch_size = 128, epochs = 5, validation_data = (X_val, y_val), verbose = 1)
 
 # Defining the decoders so that we can
 revsere_decoder_index = {value:key for key,value in tokenizer_decoder.word_index.items()}
 revsere_encoder_index = {value:key for key,value in tokenizer_encoder.word_index.items()}
 
 y_test_pred = model.predict(test_input_data[:])
-result = []
 print(len(test_input_data))
 for i in range(len(test_input_data)):
-    result.append(print_results(test_input_seqs[i], y_test_pred[i], revsere_decoder_index))
-
-df = pd.DataFrame(data={'id':test_df['id'], 'expected':result})
-df.to_csv('prediction.csv', index=False)
+    print_results(test_input_seqs[i], y_test_pred[i], revsere_decoder_index)
