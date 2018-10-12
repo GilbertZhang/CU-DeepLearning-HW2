19ce8890185f6e8680db09105dd7cb6a38ea3633
Gilbert <gilbert@Gilberts-MacBook-Pro.local>
2018-10-11 23:25:41 -0400

    add cross validation for unet

--- hw2unet.py
+++ hw2unet.py
@@ -12,6 +12,7 @@ from keras import backend as K
 import tensorflow as tf
 from keras import optimizers
 from keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import KFold
 
 
 # The custom accuracy metric used for this task
@@ -81,108 +82,148 @@ test_input_data = sequence.pad_sequences(test_input_data, maxlen = maxlen_seq, p
 n_words = len(tokenizer_encoder.word_index) + 1
 n_tags = len(tokenizer_decoder.word_index) + 1
 
-input = Input(shape = (maxlen_seq,))
+def get_model():
+    input = Input(shape = (maxlen_seq,))
 
-# Defining an embedding layer mapping from the words (n_words) to a vector of len 128
-x = Embedding(input_dim = n_words, output_dim = 128, input_length = maxlen_seq)(input)
-conv1 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(x)
-conv1 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-conv1 = BatchNormalization()(conv1)
-conv1 = Dropout(0.4)(conv1)
-pool1 = MaxPooling1D(pool_size=(2))(conv1)
+    # Defining an embedding layer mapping from the words (n_words) to a vector of len 128
+    x = Embedding(input_dim = n_words, output_dim = 128, input_length = maxlen_seq)(input)
+    conv1 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(x)
+    conv1 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Dropout(0.4)(conv1)
+    pool1 = MaxPooling1D(pool_size=(2))(conv1)
 
-conv2 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-conv2 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-conv2 = BatchNormalization()(conv2)
-conv2 = Dropout(0.4)(conv2)
-pool2 = MaxPooling1D(pool_size=(2))(conv2)
+    conv2 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Dropout(0.4)(conv2)
+    pool2 = MaxPooling1D(pool_size=(2))(conv2)
 
-conv3 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-conv3 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-conv3 = BatchNormalization()(conv3)
-conv3 = Dropout(0.4)(conv3)
-pool3 = MaxPooling1D(pool_size=(2))(conv3)
-
-
-conv4 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-conv4 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-conv4 = BatchNormalization()(conv4)
-drop4 = Dropout(0.4)(conv4)
-pool4 = MaxPooling1D(pool_size=(2))(drop4)
-
-conv5 = Conv1D(2048, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-conv5 = Conv1D(2048, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-conv5 = BatchNormalization()(conv5)
-drop5 = Dropout(0.4)(conv5)
-
-up6 = Conv1D(1024, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(drop5))
-up6 = BatchNormalization()(up6)
-merge6 = concatenate([drop4,up6], axis = 2)
-merge6 = Dropout(0.3)(merge6)
-conv6 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-conv6 = BatchNormalization()(conv6)
-conv6 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-conv6 = BatchNormalization()(conv6)
-
-up7 = Conv1D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv6))
-up7 = BatchNormalization()(up7)
-merge7 = concatenate([conv3,up7], axis = 2)
-merge7 = Dropout(0.3)(merge7)
-conv7 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-conv7 = BatchNormalization()(conv7)
-conv7 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-conv7 = BatchNormalization()(conv7)
-
-up8 = Conv1D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv7))
-up8 = BatchNormalization()(up8)
-merge8 = concatenate([conv2,up8], axis = 2)
-merge8 = Dropout(0.3)(merge8)
-conv8 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-conv8 = BatchNormalization()(conv8)
-conv8 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-conv8 = BatchNormalization()(conv8)
-
-up9 = Conv1D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv8))
-up9 = BatchNormalization()(up9)
-merge9 = concatenate([conv1,up9], axis = 2)
-merge9 = Dropout(0.3)(merge9)
-conv9 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-conv9 = BatchNormalization()(conv9)
-conv9 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-conv9 = BatchNormalization()(conv9)
-
-concat = Dense(32, activation = 'relu')(conv9)
-y = Dense(n_tags, activation = 'softmax')(concat)
+    conv3 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Dropout(0.4)(conv3)
+    pool3 = MaxPooling1D(pool_size=(2))(conv3)
 
 
-# Defining the model as a whole and printing the summary
-model = Model(input, y)
-model.summary()
+    conv4 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    drop4 = Dropout(0.4)(conv4)
+    pool4 = MaxPooling1D(pool_size=(2))(drop4)
 
-adam = optimizers.Adam(lr=0.01)
-# Setting up the model with categorical x-entropy loss and the custom accuracy function as accuracy
-model.compile(optimizer = adam, loss = "categorical_crossentropy", metrics = ["accuracy", accuracy])
+    conv5 = Conv1D(2048, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv1D(2048, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    drop5 = Dropout(0.4)(conv5)
 
-# Splitting the data for train and validation sets
-X_train, X_val, y_train, y_val = train_test_split(train_input_data, train_target_data, test_size = .1, random_state = 0)
+    up6 = Conv1D(1024, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(drop5))
+    up6 = BatchNormalization()(up6)
+    merge6 = concatenate([drop4,up6], axis = 2)
+    merge6 = Dropout(0.3)(merge6)
+    conv6 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+    conv6 = BatchNormalization()(conv6)
 
-filepath="weights.best.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
+    up7 = Conv1D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv6))
+    up7 = BatchNormalization()(up7)
+    merge7 = concatenate([conv3,up7], axis = 2)
+    merge7 = Dropout(0.3)(merge7)
+    conv7 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+    conv7 = BatchNormalization()(conv7)
 
-# Training the model on the training data and validating using the validation set
-model.fit(X_train, y_train, batch_size = 64, epochs = 20, validation_data = (X_val, y_val), callbacks=callbacks_list, verbose = 1)
+    up8 = Conv1D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv7))
+    up8 = BatchNormalization()(up8)
+    merge8 = concatenate([conv2,up8], axis = 2)
+    merge8 = Dropout(0.3)(merge8)
+    conv8 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv8 = BatchNormalization()(conv8)
 
-# Defining the decoders so that we can
-revsere_decoder_index = {value:key for key,value in tokenizer_decoder.word_index.items()}
-revsere_encoder_index = {value:key for key,value in tokenizer_encoder.word_index.items()}
+    up9 = Conv1D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv8))
+    up9 = BatchNormalization()(up9)
+    merge9 = concatenate([conv1,up9], axis = 2)
+    merge9 = Dropout(0.3)(merge9)
+    conv9 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = BatchNormalization()(conv9)
 
-model.load_weights("weights.best.hdf5")
-y_test_pred = model.predict(test_input_data[:])
-result = []
-print(len(test_input_data))
-for i in range(len(test_input_data)):
-    result.append(print_results(test_input_seqs[i], y_test_pred[i], revsere_decoder_index))
+    concat = Dense(32, activation = 'relu')(conv9)
+    y = Dense(n_tags, activation = 'softmax')(concat)
 
-df = pd.DataFrame(data={'id':test_df['id'], 'expected':result})
-df.to_csv('prediction.csv', index=False)
+
+    # Defining the model as a whole and printing the summary
+    model = Model(input, y)
+    model.summary()
+
+    adam = optimizers.Adam(lr=0.01)
+    # Setting up the model with categorical x-entropy loss and the custom accuracy function as accuracy
+    model.compile(optimizer = adam, loss = "categorical_crossentropy", metrics = ["accuracy", accuracy])
+    
+    return model
+
+cross_val = False
+if not cross_val:
+    model = get_model()
+    # Splitting the data for train and validation sets
+    X_train, X_val, y_train, y_val = train_test_split(train_input_data, train_target_data, test_size = .1, random_state = 0)
+
+    filepath="weights.best.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    callbacks_list = [checkpoint]
+
+    # Training the model on the training data and validating using the validation set
+    model.fit(X_train, y_train, batch_size = 64, epochs = 20, validation_data = (X_val, y_val), callbacks=callbacks_list, verbose = 1)
+
+    # Defining the decoders so that we can
+    revsere_decoder_index = {value:key for key,value in tokenizer_decoder.word_index.items()}
+    revsere_encoder_index = {value:key for key,value in tokenizer_encoder.word_index.items()}
+
+    model.load_weights("weights.best.hdf5")
+    y_test_pred = model.predict(test_input_data[:])
+    result = []
+    print(len(test_input_data))
+    for i in range(len(test_input_data)):
+        result.append(print_results(test_input_seqs[i], y_test_pred[i], revsere_decoder_index))
+
+    df = pd.DataFrame(data={'id':test_df['id'], 'expected':result})
+    df.to_csv('prediction.csv', index=False)
+
+else:
+    def get_callbacks(name_weights, patience_lr):
+        mcp_save = ModelCheckpoint(name_weights, save_best_only=True, monitor='val_loss', mode='min')
+        # reduce_lr_loss = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=patience_lr, verbose=1, epsilon=1e-4, mode='min')
+        return [mcp_save]
+    k=10
+    cv_acc = []
+    cv_loss = []
+    folds = list(KFold(n_splits=k, shuffle=True, random_state=1).split(train_input_data, train_target_data))
+    for j, (train_idx, val_idx) in enumerate(folds):
+
+        print('\nFold ',j)
+        X_train_cv = train_input_data[train_idx]
+        y_train_cv = train_target_data[train_idx]
+        X_valid_cv = train_input_data[val_idx]
+        y_valid_cv= train_target_data[val_idx]
+        
+        name_weights = "final_model_fold" + str(j) + "_weights.h5"
+        callbacks = get_callbacks(name_weights = name_weights, patience_lr=10)
+        model = get_model()
+        model.fit(X_train_cv, y_train_cv,
+                    batch_size = 128,
+                    epochs=15,
+                    shuffle=True,
+                    verbose=1,
+                    validation_data = (X_valid_cv, y_valid_cv),
+                    callbacks = callbacks)
+        
+        loss, _, acc = model.evaluate(X_valid_cv, y_valid_cv)
+        print(loss, acc)
+        cv_acc.append(acc)
+        cv_loss.append(loss)
+    print("Average Accuracy: {}, Average Loss: {}".format(np.mean(cv_acc), np.mean(cv_loss)))
