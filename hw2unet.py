c606431d2b0d99064a4fa789db44ab8167aba6a5
Gilbert <gilbert@Gilberts-MacBook-Pro.local>
2018-10-11 21:04:59 -0400

    unet

--- hw2unet.py
+++ hw2unet.py
@@ -45,7 +45,7 @@ def seq2ngrams(seqs, n = 3):
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 
-maxlen_seq = 512
+maxlen_seq = 1024
 
 # Loading and converting the inputs to trigrams
 train_input_seqs, train_target_seqs = train_df[['input', 'expected']][(train_df.len <= maxlen_seq)].values.T
@@ -85,44 +85,65 @@ input = Input(shape = (maxlen_seq,))
 x = Embedding(input_dim = n_words, output_dim = 128, input_length = maxlen_seq)(input)
 conv1 = Conv1D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(x)
 conv1 = Conv1D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+conv1 = BatchNormalization()(conv1)
+conv1 = Dropout(0.5)(conv1)
 pool1 = MaxPooling1D(pool_size=(2))(conv1)
-pool1 = Dropout(0.5)(pool1)
 
 conv2 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
 conv2 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+conv2 = BatchNormalization()(conv2)
+conv2 = Dropout(0.5)(conv2)
 pool2 = MaxPooling1D(pool_size=(2))(conv2)
 
 conv3 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
 conv3 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+conv3 = BatchNormalization()(conv3)
+conv3 = Dropout(0.5)(conv3)
 pool3 = MaxPooling1D(pool_size=(2))(conv3)
+
+
 conv4 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
 conv4 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+conv4 = BatchNormalization()(conv4)
 drop4 = Dropout(0.5)(conv4)
 pool4 = MaxPooling1D(pool_size=(2))(drop4)
 
 conv5 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
 conv5 = Conv1D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+conv5 = BatchNormalization()(conv5)
 drop5 = Dropout(0.5)(conv5)
 
 up6 = Conv1D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(drop5))
+up6 = BatchNormalization()(up6)
 merge6 = concatenate([drop4,up6], axis = 2)
 conv6 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+conv6 = BatchNormalization()(conv6)
 conv6 = Conv1D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+conv6 = BatchNormalization()(conv6)
 
 up7 = Conv1D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv6))
+up7 = BatchNormalization()(up7)
 merge7 = concatenate([conv3,up7], axis = 2)
 conv7 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+conv7 = BatchNormalization()(conv7)
 conv7 = Conv1D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+conv7 = BatchNormalization()(conv7)
 
 up8 = Conv1D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv7))
+up8 = BatchNormalization()(up8)
 merge8 = concatenate([conv2,up8], axis = 2)
 conv8 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+conv8 = BatchNormalization()(conv8)
 conv8 = Conv1D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+conv8 = BatchNormalization()(conv8)
 
 up9 = Conv1D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = (2))(conv8))
+up9 = BatchNormalization()(up9)
 merge9 = concatenate([conv1,up9], axis = 2)
 conv9 = Conv1D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+conv9 = BatchNormalization()(conv9)
 conv9 = Conv1D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+conv9 = BatchNormalization()(conv9)
 
 flatten = Dropout(0.5)(conv9)
 concat = Dense(64, activation = 'relu')(flatten)
@@ -157,7 +178,7 @@ model.compile(optimizer = "rmsprop", loss = "categorical_crossentropy", metrics 
 X_train, X_val, y_train, y_val = train_test_split(train_input_data, train_target_data, test_size = .1, random_state = 0)
 
 # Training the model on the training data and validating using the validation set
-model.fit(X_train, y_train, batch_size = 64, epochs = 10, validation_data = (X_val, y_val), verbose = 1)
+model.fit(X_train, y_train, batch_size = 64, epochs = 20, validation_data = (X_val, y_val), verbose = 1)
 
 # Defining the decoders so that we can
 revsere_decoder_index = {value:key for key,value in tokenizer_decoder.word_index.items()}
